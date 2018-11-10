578406794716831cbf4e5020f8f8c0b3474089fe
Cooper Kunz <cooperakunz@gmail.com>
2018-11-09 18:14:10 -0700

    Stage SEO content

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import config from '../../config/website';
 
+import MetaImage from '../images/meta-image.png'
+import Favicon from '../../static/favicons/favicon.png'
+
 const SEO = () => {
   const title = config.siteTitle;
   const description = config.siteDescription;
@@ -22,10 +25,19 @@ const SEO = () => {
       <html lang={config.siteLanguage} />
       <title>Hack Arizona | The largest hackathon in the Southwest</title>
       <meta name="description" content='Hack Arizona is the largest hackathon in the Southwest. January 18-20, 2019. Learn more and register now!' />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/hacktus-favicon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href={Favicon} />
+      <link rel="icon" type="image/png" sizes='16x16' href={Favicon} />
       <meta property="og:locale" content={config.ogLanguage} />
       <meta property="og:site_name" content='Hack Arizona' />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={MetaImage} />
+      <meta property="og:title" content="Hack Arizona | The largest hackathon in the Southwest"></meta>
+      <meta property="og:description" content='Hack Arizona is the largest hackathon in the Southwest. January 18-20, 2019. Learn more and register now!' />
+      <meta property="og:image" content={MetaImage}  />
+      <meta property="og:url" content="https://hackarizona.org" />
+      <meta name="twitter:title" content="Hack Arizona | The largest hackathon in the Southwest "></meta>
+      <meta name="twitter:description" content='Hack Arizona is the largest hackathon in the Southwest. January 18-20, 2019. Learn more and register now!'/>
+      <meta name="twitter:image" content={MetaImage} />
+      <meta name="twitter:card" content={MetaImage}  />
       {/* 
       <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
       <link rel="shortcut icon" href="/favicons/favicon.ico" />
@@ -33,17 +45,6 @@ const SEO = () => {
       <meta name="msapplication-config" content="browserconfig.xml" />
       <meta name="description" content={description} />
       <script type="application/ld+json">{JSON.stringify(schemaOrgJSONLD)}</script>
-      <meta property="og:locale" content={config.ogLanguage} />
-      <meta property="og:site_name" content={config.ogSiteName} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta property="fb:app_id" content={config.siteFBAppID ? config.siteFBAppID : ''} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content={config.userTwitter ? config.userTwitter : ''} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
       */}
     </Helmet>
   );
